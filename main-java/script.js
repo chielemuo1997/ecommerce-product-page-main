1aac1b47741db61141926cf3b4291e24ec02997b
Chinecherem Elemuo <chielemuo1997@gmail.com>
2023-12-02 01:03:09 +0100

    concluded

---
+++ main-java/script.js
@@ -0,0 +1,11 @@
+const products = document.getElementsByClassName("products")
+const bottomItems = document.getElementsByClassName("bottom-items")
+const addToCart = document.getElementsByClassName("add")
+
+addToCart.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    function addToCart() {
+        
+    }
+})
